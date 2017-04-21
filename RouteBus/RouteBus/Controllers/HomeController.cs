6f48ea26bd8d7a77ee3d233e990e6d2049ebeb1e
kristijaan <kiko_momiroski@hotmail.com>
2017-04-21 19:47:26 +0200

    modifikacii i dodaden Najava Layot

--- RouteBus/RouteBus/Controllers/HomeController.cs
+++ RouteBus/RouteBus/Controllers/HomeController.cs
@@ -26,5 +26,6 @@ namespace RouteBus.Controllers
 
             return View();
         }
+
     }
 }
---
+++ RouteBus/RouteBus/Models/Najava.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RouteBus.Models
+{
+    public class Najava
+    {
+        
+    }
+}
